ceb5d71cc9ae9993057680675cd839d22f1896c5
SuryaPrasanth1437 <37958227+SuryaPrasanth1437@users.noreply.github.com>
2019-01-03 17:17:56 +0530

    Create Maths.java

---
+++ Maths.java
@@ -0,0 +1,92 @@
+package oops;
+
+public class Maths {
+
+	public static void main(String[] args) 
+	{
+	double x=28;
+	double y=4;
+	System.out.println("maximum number of x and y is:"+Math.max(x,y));
+	System.out.println("square root of y is:"+Math.sqrt(y));
+	System.out.println("the power of x and y is"+Math.pow(x,y));
+	
+	System.out.println("logarithm of x is:"+Math.log(x));
+	System.out.println("logarithm of y is :"+Math.log(y));
+	
+	System.out.println("log10 of x is:"+Math.log10(x));
+	System.out.println("log10 of y is "+Math.log10(y));
+	System.out.println("log1px of x is"+Math.log1p(x));
+	
+	System.out.println("exp x of x is :"+Math.exp(x));
+	System.out.println("exp1p of x is:"+Math.expm1(x));
+	
+	System.out.println("*******************************************************");
+	
+	
+	double a=30;
+	double b=Math.toRadians(a);
+	System.out.println("the radian value for a is :"+b);
+	
+	System.out.println("*******************************************************");
+	
+	System.out.println("sin of a is:"+Math.sin(a));
+	System.out.println("cos of a is :"+Math.cos(a));
+	System.out.println("tan of a is :"+Math.tan(a));
+	
+	
+	System.out.println("*******************************************************");
+	
+	
+	
+	System.out.println("arc sin of a is :"+Math.asin(a));
+	System.out.println("arc cos of a is :"+Math.acos(a));
+	System.out.println("arc tan of a is :"+Math.atan(a));
+	
+	System.out.println("*******************************************************");
+	
+	
+	System.out.println("hyperbolic sin of a is :"+Math.sinh(a));
+	System.out.println("hyperbolic cos of a is :"+Math.cosh(a));
+	System.out.println("hyperbolic tan of a is :"+Math.tanh(a));
+	
+	System.out.println("*******************************************************");
+	
+	
+	System.out.println("the absolute value is :"+Math.abs(-5));
+	
+	System.out.println("the maximum value is :"+Math.max(48, 58));
+	
+	System.out.println("the minimum value is :"+Math.min(48, 58));
+	
+	System.out.println("the rounded value is :"+Math.round(98.48));
+	
+	System.out.println("the square root value is :"+Math.sqrt(4));
+	
+	System.out.println("the cube rooot value is :"+Math.cbrt(8));
+	
+	System.out.println("the power value is"+Math.pow(2,3));
+	
+	System.out.println("the sign of the given value is :"+Math.signum(-4));
+	
+	System.out.println("the ceil value is :"+Math.ceil(48.45));
+	
+	System.out.println("the copy sign value is :"+Math.copySign(45, -45));
+	
+	System.out.println("the next after value is :"+Math.nextAfter(48.3, 49.5));
+	
+	System.out.println("the next up value is:"+Math.nextUp(48.4));
+	
+	System.out.println("the next down value is :"+Math.nextDown(48.4));
+	
+	System.out.println("the floor value is :"+Math.floor(48.5));
+	
+	System.out.println("the floor division value is :"+Math.floorDiv(48, 11));
+	
+	
+	
+			
+	
+	
+	}
+
+}
